54e74e511c85a184a07f52590a88c5b3a87a7b86
JavascriptFTW <joshuamagma2013@gmail.com>
2015-07-24 17:47:06 -0500

    Work on games
    Work on a basic Tic Tac Toe game to be played by people in chat. As of
    yet accepting challenges to a game does not work. Also did some other
    miscellaneous work.

--- bot.js
+++ bot.js
@@ -11,6 +11,9 @@ var Bot = (function() {
 	var submit = $('input[type="submit"]');
 	var inChat = $(".roster-pane");
 
+	var locationSplit = window.location.toString().split("/");
+	var owner = locationSplit[locationSplit.length - 2];
+
 	clearInterval(window.update);
 
 	var body = document.body;
@@ -30,7 +33,7 @@ var Bot = (function() {
 
 	var commandUtil = {
 		commands: commands,
-		owner: $('.chat-heading div').text().replace('Chat: ', ''),
+		owner: window.location,
 		inChat: []
 	};
 
@@ -204,6 +207,11 @@ var Bot = (function() {
 		internal.onMessage.push(callback.bind(commandUtil));
 	}
 
+	Bot.inChat = function(user) {
+		if (commandUtil.inChat.indexOf(user) !== -1) return true;
+		return false;
+	}
+
 	Bot.sendMessage = internal.sendMessage;
 
 
@@ -241,7 +249,7 @@ var Bot = (function() {
 	            notify(username + " has joined the stream!");
 	            if (Bot.welcome.constructor === Array) {
 	            	var messageIndex = Math.floor(Math.random() * Bot.welcome.length)
-					internal.sendMessage(Bot.welcome[welcomeIndex], {target: username});
+					internal.sendMessage(Bot.welcome[messageIndex], {target: username});
 	            } else {
 	            	internal.sendMessage(Bot.welcome, {target: username});
 	        	}
@@ -259,7 +267,7 @@ var Bot = (function() {
 			console.log(this.owner);
 			if (parsedMsg.body[0] === "/") {
 				internal.runCommand(parsedMsg.body.substr(1), parsedMsg.sender);
-			} else if(parsedMsg.body.indexOf("@" + this.owner) !== -1) {
+			} else if(!document.hasFocus()) {
 				notify(parsedMsg.sender + ": " + parsedMsg.body);
 			}
 		}
@@ -295,6 +303,16 @@ var Bot = (function() {
 
 	return Bot;
 })();
+
+
+
+Bot.welcome = [
+	"{{SENDER}}: Welcome to the stream @{{TARGET}}!",
+	"{{SENDER}}: Hi @{{TARGET}}! Thanks for tuning in!",
+	"{{SENDER}}: Hello @{{TARGET}}!"
+];
+
+
 
 Bot.setCommand("shrug", function() {
 	var data = arguments[arguments.length - 1];
@@ -343,13 +361,16 @@ Bot.setCommand("log", function(a, b) {
 	sendMessage("{{SENDER}}: @{{TARGET}} the base " + b + " log of " + a + " equals " + (Math.log(a) / Math.log(b)), {target: data.sender});
 }, "Gets the logarithm of a number. Usage /log number base");
 
+
+
 Bot.setCommand("roster", function() {
-	sendMessage("There are " + inChat.length + " people in chat right now");
+	var data = arguments[arguments.length - 1];
+	sendMessage("@{{TARGET}} There are " + inChat.length + " people in chat right now", {target: data.sender});
 }, "Gets the number of people in the chatroom currently. Usage /roster");
 
 Bot.setCommand("who", function() {
 	var data = arguments[arguments.length - 1];
-	var message = "@{{TARGET}}: Users in chat are, ";
+	var message = "@{{TARGET}}: Users in chat are ";
 	for (var i = 0; i < inChat.length; i ++) {
 		var userAppend = "";
 		if (i === inChat.length - 1) {
@@ -366,4 +387,165 @@ Bot.setCommand("who", function() {
 
 
 
+
+
+
+var tttGames = {};
+
+var TicTacToe = function(cfg) {
+	if (!(this instanceof TicTacToe)) return new TicTacToe(cfg);
+	//this.id = cfg.id;
+	this.players = {
+		"X": cfg.player1,
+		"O": cfg.player2
+	};
+	this.board = [
+		["", "", ""],
+		["", "", ""],
+		["", "", ""]
+	];
+}
+
+TicTacToe.prototype = {
+	tryMove: function(player, x, y) {
+		if (x > 0 && y > 0 && x < this.board.length && y < this.board.length && this.board[x][y] === "" && player in this.players) {
+			this.board[x][y] = player;
+			this.checkEnd();
+		}
+	},
+	checkEnd: function() {
+
+	}
+};
+
+
+
+var challenges = {};
+/*
+{
+	userName: {
+		challenger: {
+			game: game,
+			from: userName
+		}
+	}
+}
+*/
+
+/*Bot.setCommand("game", function(game, opponent) {
+	var data = arguments[arguments.length - 1];
+	if (data === opponent || data === game) {
+		return;
+	}
+	/* TODO(Mr Magma): Make this better eventually when there are more games with an object of games 
+	if (game === "TTT") {
+		challenges[opponent] = {
+			game: game,
+			challenger: opponent
+		};
+	}
+});*/
+
+function sendChallenge(challengee, game, data) {
+	if (Bot.inChat(challengee)) {
+		if (challenges[challengee] !== undefined && !(data.sender in challenges[challengee])) {
+			challenges[challengee][data.sender] = {
+				from: data.sender,
+				game: "TTT"
+			}
+		} else if (challenges[challengee] === undefined) {
+			challenges[challengee] = {};
+			challenges[challengee][data.sender] = {
+				from: data.sender,
+				game: "TTT"
+			}
+		} else {
+			Bot.sendMessage("@{{PLAYER1}} you have already sent a challenge to @{{PLAYER2}}! Please wait for them to respond!", {
+				player1: data.sender,
+				player2: challengee
+			})
+			return;
+		}
+		Bot.sendMessage("@{{PLAYER1}} has challenged @{{PLAYER2}} to a game of Tic Tac Toe! ", {
+			player1: data.sender,
+			player2: challengee
+		});
+	} else {
+		Bot.sendMessage("@{{PLAYER1}}: {{PLAYER2}} is not in the chat right now", {
+			player1: data.sender,
+			player2: challengee
+		});
+	}
+}
+
+function declineChallenge(challenger, data) {
+	if (challenges[data.sender] !== undefined) {
+		delete challenges[data.sender][challenger];
+		Bot.sendMessage("@{{PLAYER1}} has declined @{{PLAYER2}}'s challenge!", {
+			player1: data.sender,
+			player2: challenger
+		})
+	}
+}
+
+function acceptChallenge(challenger, data) {
+	var challengee = challenges[data.sender];
+	if (challengee[challenger] !== undefined) {
+		var challenge = challengee[challenger].game;
+		if (challenge === "ttt") {
+			tttGames[challenger] = new TicTacToe({
+				player1: challenger,
+				player2: data.sender
+			});
+			delete challengee[challenger];
+			Bot.sendMessage("@{{PLAYER1}} has accepted @{{PLAYER2}}'s challenge to a game of Tic Tac Toe!", {
+				player1: challenger,
+				player2: data.sender
+			})
+		}
+	}
+}
+
+function listChallenges(data) {
+	//Tell the user all challenges that have been sent to them
+	if (challenges[data.sender] === undefined) {
+		sendMessage("@{{TARGET}}: You do not have any challenges currently.", {
+			target: data.sender
+		})
+	} else {
+		var userChallenges = challenges[data.sender];
+		var challengers = [];
+		for (var i in userChallenges) {
+			challengers.push(i);
+		}
+		if (!challengers.length) {
+			sendMessage("@{{TARGET}}: You have challenges from " + challengers.join(", "), {
+				target: data.sender
+			})
+		} else {
+			sendMessage("@{{TARGET}}: You do not have any challenges currently.", {
+				target: data.sender
+			})
+		}
+	}
+}
+
+Bot.setCommand("challenge", function() {
+	var data = arguments[arguments.length - 1];
+	if (arguments.length === 1) {
+		listChallenges(data.sender);
+	} else if (arguments.length === 3) {
+		if (arguments[0] === "accept" && challenges[data.sender] !== undefined) {
+			acceptChallenge(arguments[1], data);
+		} else if (arguments[0] === "decline") {
+			declineChallenge(arguments[1], data);
+		} else {
+			sendChallenge(arguments[0], arguments[1], data);
+		}
+	}
+}, "Used to challenge other people to games, check your challenges, and accept and decline challenges. Usage /challenge to get your challenges, /challenge user game to challenge someone to a game, and /challenge <accept|decline> user to accept or decline challenges");
+
+Bot.setCommand("game", function() {
+	var data = arguments[arguments.length - 1];
+});
 
